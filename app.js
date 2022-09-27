32180c5db8ac752dd471e2568008c738bf88fad1
Clinton Philathong <cphilathong1@gmail.com>
2022-09-27 15:42:45 +1300

    add on and off endpoints

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
+const express = require("express");
+const app = express();
+const expressPort = 3000;
 const { SerialPort } = require("serialport");
 
-// MAC
+//MAC
 const port = new SerialPort({ path: "/dev/tty.usbmodem14402", baudRate: 9600 });
 
 // WINDOWS
 //const port = new SerialPort({ path: "COM3", baudRate: 9600 });
 
-setTimeout(() => {
-  port.write("0", function (err) {
-    if (err) {
-      return console.log("Error on write: ", err.message);
-    }
-    console.log("message written");
-  });
-}, 1000);
+app.get("/on", (req, res) => {
+  setTimeout(() => {
+    port.write("1", function (err) {
+      if (err) {
+        return console.log("Error on write: ", err.message);
+      }
+      console.log("message written");
+    });
+  }, 1000);
+});
+
+app.get("/off", (req, res) => {
+  setTimeout(() => {
+    port.write("0", function (err) {
+      if (err) {
+        return console.log("Error on write: ", err.message);
+      }
+      console.log("message written");
+    });
+  }, 1000);
+});
+
+app.listen(expressPort, () => {
+  console.log(`Listening on port ${expressPort}`);
+});
