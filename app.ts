593eb1573b45bb2d3a2104a08e20d0411e8c65f5
Clinton Philathong <cphilathong1@gmail.com>
2022-10-11 04:13:32 +1300

    add socket io

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
+import { Request, Response } from "express";
 const express = require("express");
 const bodyParser = require("body-parser");
 const { SerialPort } = require("serialport");
-import { Request, Response } from "express";
 
 const app = express();
 const expressPort = 3000;
@@ -9,22 +9,32 @@ const expressPort = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const server = app.listen(expressPort, () => {
+  console.log(`Listening on port ${expressPort}`);
+});
+
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log("connection works!!" + socket.id);
+});
+
 //MAC
-const port = new SerialPort({ path: "/dev/tty.usbmodem14402", baudRate: 9600 });
+//const port = new SerialPort({ path: "/dev/tty.usbmodem14402", baudRate: 9600 });
 
 // WINDOWS
 //const port = new SerialPort({ path: "COM3", baudRate: 9600 });
 
-app.get("/on", (req: Request, res: Response) => {
-  setTimeout(() => {
-    port.write("1", function (err: Error) {
-      if (err) {
-        console.log("Error on write: ", err.message);
-      }
-      console.log("message written");
-    });
-  }, 1000);
-});
+// app.get("/on", (req: Request, res: Response) => {
+//   setTimeout(() => {
+//     port.write("1", function (err: Error) {
+//       if (err) {
+//         console.log("Error on write: ", err.message);
+//       }
+//       console.log("message written");
+//     });
+//   }, 1000);
+// });
 
 // app.get("/off", (req, res) => {
 //   setTimeout(() => {
@@ -37,30 +47,35 @@ app.get("/on", (req: Request, res: Response) => {
 //   }, 1000);
 // });
 
-// send desired voltage
+// get 5 samples of vout
 
-app.get("/refresh", (req: Request, res: Response) => {
-  const samples = [];
-});
+// app.get("/refresh", (req: Request, res: Response) => {
+//   const samples = [];
+
+//   setTimeout(() => {
+//     port.on("data", (sample: number) => samples.push(sample));
+//   }, 2000);
+
+//   res.json({ vout: samples });
+// });
+
+// send desired voltage
 
 app.post("/voltage", (req: Request, res: Response) => {
   const desiredVoltage = req.body.voltage;
 
   console.log(`The desired voltage ${desiredVoltage}`);
 
-  setTimeout(() => {
-    port.write(desiredVoltage, (err: Error) => {
-      if (err) {
-        console.log("Error on write: ", err.message);
-      }
-    });
-  }, 500);
-
-  res.sendStatus(200);
-});
-
-app.listen(expressPort, () => {
-  console.log(`Listening on port ${expressPort}`);
+  // if (desiredVoltage >= 0 && desiredVoltage <= 30) {
+  //   setTimeout(() => {
+  //     port.write(desiredVoltage, (err: Error) => {
+  //       if (err) {
+  //         console.log("Error on write: ", err.message);
+  //       }
+  //     });
+  //     res.sendStatus(200);
+  //   }, 500);
+  // }
 });
 
 function normaliseVoltage(desiredVoltage: number) {
