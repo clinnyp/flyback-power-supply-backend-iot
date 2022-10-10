5b00fd9806a5d0e8e07a5840607296dd9fa8e363
Clinton Philathong <cphilathong1@gmail.com>
2022-10-10 15:17:02 +1300

    use parser

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const app = express();
-const expressPort = 3000;
+const bodyParser = require("body-parser");
 const { SerialPort } = require("serialport");
 
+const app = express();
+const expressPort = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 //MAC
-const port = new SerialPort({ path: "/dev/tty.usbmodem14402", baudRate: 9600 });
+//const port = new SerialPort({ path: "/dev/tty.usbmodem14402", baudRate: 9600 });
 
 // WINDOWS
 //const port = new SerialPort({ path: "COM3", baudRate: 9600 });
@@ -13,24 +18,50 @@ app.get("/on", (req, res) => {
   setTimeout(() => {
     port.write("1", function (err) {
       if (err) {
-        return console.log("Error on write: ", err.message);
+        console.log("Error on write: ", err.message);
       }
       console.log("message written");
     });
   }, 1000);
 });
 
-app.get("/off", (req, res) => {
+// app.get("/off", (req, res) => {
+//   setTimeout(() => {
+//     port.write("0", function (err) {
+//       if (err) {
+//         console.log("Error on write: ", err.message);
+//       }
+//       console.log("message written");
+//     });
+//   }, 1000);
+// });
+
+// send desired voltage
+
+app.get("/refresh", (req, res) => {
+  const samples = [];
+});
+
+app.post("/voltage", (req, res) => {
+  const desiredVoltage = req.body.voltage;
+
+  console.log(`The desired voltage ${desiredVoltage}`);
+
   setTimeout(() => {
-    port.write("0", function (err) {
+    port.write(desiredVoltage, (err) => {
       if (err) {
-        return console.log("Error on write: ", err.message);
+        console.log("Error on write: ", err.message);
       }
-      console.log("message written");
     });
-  }, 1000);
+  }, 500);
+
+  res.sendStatus(200);
 });
 
 app.listen(expressPort, () => {
   console.log(`Listening on port ${expressPort}`);
 });
+
+function normaliseVoltage(desiredVoltage) {
+  desiredVoltage;
+}
